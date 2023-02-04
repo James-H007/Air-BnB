7a69b3562725475bcd9f6a989817f1e350bec6e4
James Hoang <hoangjames23@gmail.com>
2023-02-04 13:28:44 -0800

    Authenticate-Me-Phase-1and2

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
